4ade18545619e328600e93ccc955f664583f8a58
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-05-17 15:37:42 +0300

    move to quarto

--- _common.R
+++ _common.R
@@ -1,15 +1,6 @@
-options(tidyverse.quiet = TRUE)
 
-knitr::opts_chunk$set(
-  cache = TRUE,
-  out.width = "100%",
-  echo = TRUE,
-  message = FALSE,
-  warning = FALSE,
-  comment = "",
-  dev = "ragg_png",
-  paged.print = TRUE
-)
+options(tidyverse.quiet = TRUE)
+set.seed(2022)
 
 ggplot2::theme_set(ggplot2::theme_minimal())
 
